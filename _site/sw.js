5eba166c77fb2ce26bc3350be2121d99573c1c8a
Gina Roberg <ginaroberg@gmail.com>
2023-08-03 00:22:57 -0700

    removed tags

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230803.001939';
+const cacheName = 'chirpy-20230803.002249';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
