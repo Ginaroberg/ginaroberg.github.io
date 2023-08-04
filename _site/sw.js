f2e2e6c812329c1dae7e22122dc8aa2478a3a1e7
Gina Roberg <ginaroberg@gmail.com>
2023-08-04 12:54:43 -0700

    rebuilt

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230804.125226';
+const cacheName = 'chirpy-20230804.125425';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
